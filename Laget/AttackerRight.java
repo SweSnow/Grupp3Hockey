f17f629132845a09e815ab6fc0d93ed09c2841fe
Wertoyou <bjorn.thoren97@gmail.com>
2016-02-07 10:18:31 +0100

    Replaces Attacker.java with AttackerLeft.java/AttackerRight.java

--- Laget/AttackerRight.java
+++ Laget/AttackerRight.java
@@ -5,18 +5,18 @@ import hockey.api.GoalKeeper;
 import hockey.api.Player;
 import hockey.api.ITeam;
 
-public class Attacker extends Player {
+public class AttackerRight extends Player {
 
     private int index;
     private String name;
 
-    public Attacker(int index, String name) {
+    public AttackerRight(int index, String name) {
         this.index = index;
         this.name = name;
     }
     public int getNumber() { return Pucko.numbers[index]; }
     public String getName() { return name; }
-    public boolean isLeftHanded() { return false; }
+    public boolean isLeftHanded() { return true; }
     public void step() {
         if (hasPuck()) // If we have the puck
             skate(2600, 0, 1000); // Go for the goal
